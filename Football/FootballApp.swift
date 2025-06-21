a489955503507a275fda2ced395a088c2567920d
GitYourselfaBreak <itsofficialzach@gmail.com>
2025-06-21 02:15:36 -0700

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Zach P on 6/21/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
